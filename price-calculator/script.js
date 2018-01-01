abaae2b41cad45f7cb09f0ccc7243074e605ac5d
michellejanosi <michelle.janosi@gmail.com>
2018-01-01 10:03:19 -0500

    add to dos

---
+++ price-calculator/script.js
@@ -0,0 +1,12 @@
+/*
+  - goal: let users select options then show price
+  - handling price:
+    - store price in a variable
+    - price should change when an option is clicked
+  - handling options:
+    - use checkboxes for options
+    - use radio buttons when user can only select one option
+    - data can be stored in an object
+  - handling display
+    - show a list of user selected items and total price
+*/
